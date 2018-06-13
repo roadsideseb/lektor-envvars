751d4cf68193d25e70b8c7666d61ecb1116db7eb
Sebastian Vetter <seb@roadsi.de>
2018-06-12 23:21:02 -0600

    Move dev requirements into separate file

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
-import sys
-
-from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
-
-requires = ["lektor", "environs"]
-tests_requires = ["pytest", "pytest-cache", "pytest-cov"]
-lint_requires = ["flake8", "black"]
-dev_requires = requires + tests_requires + lint_requires
+from setuptools import setup
 
 
 setup(
@@ -18,8 +10,8 @@ setup(
     author="Sebastian Vetter",
     author_email="seb@roadsi.de",
     url="https://www.github.com/elbaschid/lektor-envvars",
-    packages=find_packages(),
-    install_requires=requires,
+    py_modules=["lektor_envvars"],
+    install_requires=["lektor", "environs"],
     entry_points={"lektor.plugins": ["envvars = lektor_envvars:EnvvarsPlugin"]},
     classifiers=[
         "License :: OSI Approved :: MIT License",
@@ -27,5 +19,4 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: Implementation :: CPython",
     ],
-    extras_require={"test": tests_requires, "dev": dev_requires, "lint": lint_requires},
 )
